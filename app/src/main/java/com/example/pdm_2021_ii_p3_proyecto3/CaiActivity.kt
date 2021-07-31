a48da8eab299697ad056c00933d25d923e999311
Mitsuki <angienicoll7@gmail.com>
2021-07-30 20:59:46 -0600

    initial commit

---
+++ app/src/main/java/com/example/pdm_2021_ii_p3_proyecto3/CaiActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pdm_2021_ii_p3_proyecto3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CaiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cai)
+    }
+}
---
+++ app/src/main/java/com/example/pdm_2021_ii_p3_proyecto3/TipoEmpleadoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pdm_2021_ii_p3_proyecto3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TipoEmpleadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tipo_empleado)
+    }
+}
