78d864ff506bc9292f4c662b34a797daa75ab97c
Carlos <cmchamorro99@hotmail.com>
2021-08-20 16:15:09 -0600

    Agregando validacion de vacio y minimo al login

--- app/src/main/java/com/example/pdm_2021_ii_p3_proyecto3/MainActivity.kt
+++ app/src/main/java/com/example/pdm_2021_ii_p3_proyecto3/MainActivity.kt
@@ -48,7 +48,37 @@ class MainActivity() : AppCompatActivity() {
         startActivity(intent)
     }
 
+    private fun estaVacio():Boolean{
+        if(txtUsuario.text.toString().isEmpty()) {
+            txtUsuario.error ="Debe rellenar el usuario del empleado"
+            return true
+        }
+        if(txtContraseña.text.toString().isEmpty()) {
+            txtContraseña.error ="Debe rellenar la contraseña del empleado"
+            return true
+        }
+        return false
+    }
+
+    private fun noLoSuficienteLargo():Boolean{
+        if(txtUsuario.text.toString().length < 8) {
+            txtUsuario.error ="El nombre de usuario no puede ser menor a 8 dígitos"
+            return true
+        }
+        if(txtContraseña.text.toString().length < 8) {
+            txtContraseña.error ="La contraseña no puede ser menor a 8 dígitos"
+            return true
+        }
+        return false
+    }
+
     private fun callServiceGetEmpleados() {
+        if(estaVacio()){
+            return
+        }
+        if(noLoSuficienteLargo()){
+            return
+        }
         val personService: EmpleadoService = RestEngine.buildService().create(EmpleadoService::class.java)
         var result: Call<List<EmpleadoDataCollectionItem>> = personService.listEmpleados()
         val nombreUsuario = txtUsuario.text.toString()
